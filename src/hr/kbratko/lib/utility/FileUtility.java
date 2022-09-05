e4f4f6133ae1469482b5658dc721d056a77eb52f
Karlo Bratko <karlobratko@gmail.com>
2022-09-05 19:27:29 +0200

    feat(lib): implement uploadFile and copy methods in FileUtility
    Add uploadFile method which opens file chooser and returns selected File, and add copy methods which copies File from source path to destination path.

--- src/hr/kbratko/lib/utility/FileUtility.java
+++ src/hr/kbratko/lib/utility/FileUtility.java
@@ -12,12 +12,44 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Optional;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.filechooser.FileSystemView;
 
 /**
  *
  * @author kbratko
  */
-public class FileUtility {
+public final class FileUtility {
+
+  private static final String DIALOG_UPLOAD_TEXT = "Upload";
+
+  private FileUtility() {
+  }
+
+  public static Optional<File> uploadFile(String description,
+                                          String... extensions) {
+    JFileChooser chooser =
+                 new JFileChooser(
+                   FileSystemView.getFileSystemView().getHomeDirectory());
+    chooser.setFileFilter(new FileNameExtensionFilter(description, extensions));
+    chooser.setDialogTitle(DIALOG_UPLOAD_TEXT);
+    chooser.setApproveButtonText(DIALOG_UPLOAD_TEXT);
+    chooser.setApproveButtonToolTipText(DIALOG_UPLOAD_TEXT);
+    if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+      File selectedFile = chooser.getSelectedFile();
+      String extension = selectedFile.getName()
+             .substring(selectedFile.getName().lastIndexOf(".") + 1);
+
+      return selectedFile.exists() &&
+             Arrays.asList(extensions).contains(extension.toLowerCase())
+               ? Optional.of(selectedFile)
+               : Optional.empty();
+    }
+    return Optional.empty();
+  }
 
   public static void copyFromUrl(String urlSource, String localDestination)
     throws MalformedURLException, IOException {
@@ -27,6 +59,12 @@ public class FileUtility {
     try ( InputStream is = con.getInputStream()) {
       Files.copy(is, Paths.get(localDestination));
     }
+  }
+
+  public static void copy(String source, String localDestination)
+    throws MalformedURLException, IOException {
+    createDirHierarchy(localDestination);
+    Files.copy(Paths.get(source), Paths.get(localDestination));
   }
 
   private static void createDirHierarchy(String destination)
